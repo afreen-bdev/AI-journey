5d5d9cc6b82e1b2b72f661f0c9f0135c4b0a8336
afreen-dev <afreenmoham@gmail.com>
2026-04-22 15:41:12 +0530

    Updated Variables&DataTypes file

---
+++ Python/1-Variables&DataTypes/concepts.py
@@ -0,0 +1,10 @@
+# Variables and Data Types
+a = 10          # int
+b = 10.5        # float
+c = "Hello"     # string
+d = True        # boolean
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
